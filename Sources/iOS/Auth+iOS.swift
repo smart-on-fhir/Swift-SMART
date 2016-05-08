5e2a6c37693d0bffe514e26cde3682ad25af9d21
Pascal Pfiffner <phase.of.matter@gmail.com>
2016-05-08 12:38:02 -0400

    Cherry-pick Auth+iOS from develop branch

--- Sources/iOS/Auth+iOS.swift
+++ Sources/iOS/Auth+iOS.swift
@@ -45,7 +45,8 @@ extension Auth {
 			// instantiate patient list view
 			let view = PatientListViewController(list: PatientListAll(), server: self.server)
 			view.title = self.oauth?.authConfig.ui.title
-			view.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: view, action: Selector("dismissFromModal:"))
+			let dismiss = #selector(PatientListViewController.dismissFromModal(_:))
+			view.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: view, action: dismiss)
 			view.onPatientSelect = { patient in
 				var params = parameters
 				if let patient = patient {
